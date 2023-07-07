f8f6c2a50c06738cdf0a323db05a24dbfd4261ac
Thoan Vo <thoanvtt@gmail.com>
2023-07-07 16:18:42 +0700

    update file

--- FlaskWebProject/__init__.py
+++ FlaskWebProject/__init__.py
@@ -5,6 +5,7 @@ import logging
 from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
+
 from flask_login import LoginManager
 from flask_session import Session
 
